dc5eb61fb4ac7b176de9ea59133d102fd0445e19
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-02-10 17:04:39 +0200

    Removed plain connection string from db context

--- Warehouse.Database/DatabaseContext.cs
+++ Warehouse.Database/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 
 namespace Warehouse.Database
@@ -7,11 +8,17 @@ namespace Warehouse.Database
     {
         protected readonly MongoClient _client;
         protected readonly IMongoDatabase _db;
+        private readonly IConfiguration _configuration;
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public DatabaseContext()
         {
             //change {database name} with database's name (curly brackets should not be included)
-            _client = new MongoClient("mongodb+srv://FidoDidoo100:A123123123a@fbcluster.rdkdn.mongodb.net/warehouse?retryWrites=true&w=majority");
+            _client = new MongoClient($"mongodb+srv://{_configuration["MongoCredentials:Username"]}:{_configuration["MongoCredentials:Password"]}{_configuration["MongoCredentials:Cluster"]}.rdkdn.mongodb.net/warehouse?retryWrites=true&w=majority");
             //Change database name which you want to use
             _db = _client.GetDatabase("warehouse");
         }
