9a27bac190519ebdbf610378cd233d680f0c7406
Iavor Orlyov <i.orlyov@drafkings.com>
2022-02-18 16:49:21 +0200

    Connection string edited

--- Warehouse.Database/DatabaseContext.cs
+++ Warehouse.Database/DatabaseContext.cs
@@ -16,9 +16,7 @@ namespace Warehouse.Database
         {
             _configuration = configuration;
 
-            var settings = MongoClientSettings.FromConnectionString($"mongodb+srv://{_configuration["DB_Username"]}:{_configuration["DB_Password"]}@{_configuration["DB_Cluster"]}.rdkdn.mongodb.net/warehouse?retryWrites=true&w=majority");
-            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-            _client = new MongoClient(settings);
+            _client = new MongoClient($"mongodb://{_configuration["DB_Username"]}:{_configuration["DB_Password"]}@{_configuration["DB_Cluster"]}.rdkdn.mongodb.net/futbot?retryWrites=true&w=majority&connect=replicaSet");
 
             _db = _client.GetDatabase("warehouse");
         }
