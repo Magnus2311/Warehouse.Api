67f5549053d9edf9395b1bdbe3200a646d45887a
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-03-10 17:19:40 +0200

    Added partner dto, service and repository

---
+++ Warehouse.Api/Controllers/PartnersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Warehouse.Api.Models.DTOs;
+using Warehouse.Api.Services;
+
+namespace Warehouse.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PartnersController : ControllerBase
+    {
+        private readonly PartnersService _partnersService;
+
+        public PartnersController(PartnersService partnersService)
+        {
+            _partnersService = partnersService;
+        }
+
+        [HttpPost]
+        public async Task<PartnerDTO> Add(PartnerDTO partnerDTO)
+                => await _partnersService.Add(partnerDTO);
+
+        [HttpPut]
+        public async Task Update(PartnerDTO partnerDTO)
+                => await _partnersService.Update(partnerDTO);
+
+        [HttpGet]
+        public async Task<IEnumerable<PartnerDTO>> Get()
+                => await _partnersService.Get();
+
+        [HttpDelete]
+        public async Task Delete([FromBody] string partnerId)
+                => await _partnersService.Delete(partnerId);
+    }
+}
