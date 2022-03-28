00ad8be950a0c94a818c5e8f8be1cfa5029e9d9f
Iavor Orlyov <i.orlyov@drafkings.com>
2022-03-28 13:20:40 +0300

    Added mapping for different type ids

--- Warehouse.Api/Services/Mappers/Mapper.cs
+++ Warehouse.Api/Services/Mappers/Mapper.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using MongoDB.Bson;
 using Warehouse.Api.Models.DTOs;
 using Warehouse.Database.Models;
 
@@ -8,17 +9,25 @@ namespace Warehouse.Api.Services.Mappers
     {
         public MapperProfile()
         {
-            CreateMap<ItemDTO, Item>();
-            CreateMap<Item, ItemDTO>();
+            CreateMap<ItemDTO, Item>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Id) ? ObjectId.GenerateNewId() : ObjectId.Parse(src.Id)));
+            CreateMap<Item, ItemDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
 
-            CreateMap<PartnerDTO, Partner>();
-            CreateMap<Partner, PartnerDTO>();
+            CreateMap<PartnerDTO, Partner>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Id) ? ObjectId.GenerateNewId() : ObjectId.Parse(src.Id)));
+            CreateMap<Partner, PartnerDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
 
-            CreateMap<SaleDTO, Sale>();
-            CreateMap<Sale, SaleDTO>();
+            CreateMap<SaleDTO, Sale>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Id) ? ObjectId.GenerateNewId() : ObjectId.Parse(src.Id)));
+            CreateMap<Sale, SaleDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
 
-            CreateMap<SaleItemDTO, SaleItem>();
-            CreateMap<SaleItem, SaleItemDTO>();
+            CreateMap<SaleItemDTO, SaleItem>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Id) ? ObjectId.GenerateNewId() : ObjectId.Parse(src.Id)));
+            CreateMap<SaleItem, SaleItemDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
         }
     }
 }
