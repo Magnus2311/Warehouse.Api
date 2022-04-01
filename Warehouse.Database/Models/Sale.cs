23238cf02738196e99b1ef18ed60c3a82ed423d3
Iavor Orlyov <i.orlyov@drafkings.com>
2022-04-01 11:19:31 +0300

    Added history for sales

--- Warehouse.Database/Models/Sale.cs
+++ Warehouse.Database/Models/Sale.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Warehouse.Database.Helpers;
+using Warehouse.Database.Models.Attributes;
 
 namespace Warehouse.Database.Models
 {
     public class Sale : BaseEntity
     {
+        [BindedProp]
         public IEnumerable<SaleItem> SaleItems { get; set; } = Enumerable.Empty<SaleItem>();
+        public IEnumerable<VersionedProp> SaleItems_history { get; set; }
+        [BindedProp]
         public string PartnerId { get; set; }
+        public IEnumerable<VersionedProp> PartnerId_history { get; set; }
+        [BindedProp]
         public string Description { get; set; }
+        public IEnumerable<VersionedProp> Description_history { get; set; }
+        [BindedProp]
         public decimal TotalAmount { get; set; }
+        public IEnumerable<VersionedProp> TotalAmount_history { get; set; }
     }
 }
