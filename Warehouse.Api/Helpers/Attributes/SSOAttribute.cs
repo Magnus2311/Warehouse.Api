0f3d058a76346a37fcbb97150271e52bb7740276
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-05-27 16:13:08 +0300

    Logging errors on SSO attribute

--- Warehouse.Api/Helpers/Attributes/SSOAttribute.cs
+++ Warehouse.Api/Helpers/Attributes/SSOAttribute.cs
@@ -18,36 +18,43 @@ namespace Warehouse.Api.Helpers.Attributes
 
         public async void OnAuthorization(AuthorizationFilterContext context)
         {
-            _context = context;
-            var ssoConnectionService = _context.HttpContext.RequestServices.GetService<SsoConnectionService>();
-            var appSettings = _context.HttpContext.RequestServices.GetService<AppSettings>();
-            var httpType = _context.HttpContext.Request.Method.ToUpperInvariant();
-            var accessToken = string.Empty;
-
-            if (httpType == "POST" || httpType == "PUT" || httpType == "PATCH")
+            try
             {
-                using (StreamReader reader
-                  = new StreamReader(_context.HttpContext.Request.Body, Encoding.UTF8, true, 1024, true))
+                _context = context;
+                var ssoConnectionService = _context.HttpContext.RequestServices.GetService<SsoConnectionService>();
+                var appSettings = _context.HttpContext.RequestServices.GetService<AppSettings>();
+                var httpType = _context.HttpContext.Request.Method.ToUpperInvariant();
+                var accessToken = string.Empty;
+
+                if (httpType == "POST" || httpType == "PUT" || httpType == "PATCH")
                 {
-                    var bodyStr = await reader.ReadToEndAsync();
-                    accessToken = JsonSerializer.Deserialize<JsonElement>(bodyStr).GetProperty("accessToken").GetString();
-                    _context.HttpContext.Request.Body.Position = 0;
+                    using (StreamReader reader
+                      = new StreamReader(_context.HttpContext.Request.Body, Encoding.UTF8, true, 1024, true))
+                    {
+                        var bodyStr = await reader.ReadToEndAsync();
+                        accessToken = JsonSerializer.Deserialize<JsonElement>(bodyStr).GetProperty("accessToken").GetString();
+                        _context.HttpContext.Request.Body.Position = 0;
+                    }
+                }
+
+                if (httpType == "GET")
+                {
+                    accessToken = _context.HttpContext.Request.Query["accessToken"];
+                }
+
+                if (!string.IsNullOrEmpty(accessToken))
+                {
+                    var userId = await ssoConnectionService.ValidateToken(accessToken);
+                    if (!string.IsNullOrEmpty(userId))
+                    {
+                        appSettings.UserId = new ObjectId(userId);
+                        return;
+                    }
                 }
             }
-
-            if (httpType == "GET")
+            catch (Exception ex)
             {
-                accessToken = _context.HttpContext.Request.Query["accessToken"];
-            }
-
-            if (!string.IsNullOrEmpty(accessToken))
-            {
-                var userId = await ssoConnectionService.ValidateToken(accessToken);
-                if (!string.IsNullOrEmpty(userId))
-                {
-                    appSettings.UserId = new ObjectId(userId);
-                    return;
-                }
+                Console.WriteLine(ex);
             }
 
             _context.Result = new UnauthorizedResult();
